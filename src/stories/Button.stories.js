c5825b939705faf3fe09ccdf06fc1e839096806c
gjgd <gjgd@protonmail.com>
2021-05-07 11:00:40 -0500

    chore: change and break stories

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -15,22 +15,22 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   primary: true,
-  label: 'Button',
+  label: 'Button 1',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+  label: 'Button 2',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'large',
-  label: 'Button',
+  label: 'Button 3',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'small',
-  label: 'Button',
+  label: 'Button 4 ',
 };
--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Page } from './Page';
-import * as HeaderStories from './Header.stories';
 
 export default {
   title: 'Example/Page',
@@ -11,11 +10,9 @@ export default {
 const Template = (args) => <Page {...args} />;
 
 export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  ...HeaderStories.LoggedIn.args,
-};
+LoggedIn.args = {};
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
+  user: {},
 };
